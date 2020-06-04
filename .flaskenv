42b01d79460aeb687270952a6fd178a14df9bb5e
atstpls <25083331+atstpls@users.noreply.github.com>
2020-06-04 09:24:11 -0500

    update

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=t3.py
+FLASK_DEBUG=1
--- t3.py
+++ t3.py
@@ -1,10 +1,10 @@
+from flask import Flask
 from app import create_app, db
 from app.models import User, Post, Message, Notification, Task
 
-app = create_app()
 
+app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
-            'Notification': Notification, 'Task': Task}
+    return {'db': db, 'User': User, 'Post': Post, 'Message': Message, 'Notification': Notification, 'Task': Task}
